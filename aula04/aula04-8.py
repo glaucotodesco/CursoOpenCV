67280d25cd8e7f22db78e360810f4f86c1e964ae
Glauco Todesco <glauco.todesco@gmail.con>
2020-02-28 19:27:09 -0300

    fix url and comment

--- aula04/aula04-8.py
+++ aula04/aula04-8.py
@@ -1,8 +1,8 @@
 import cv2
 
-imgRGB  = cv2.imread('../data/lena.jpg')
+imgRGB  = cv2.imread('data/lena.jpg')
 
-#crua uma borda branca de 20pixels na imagem
+#cria uma borda branca de 20pixels na imagem
 imgRGBWithBorder = cv2.copyMakeBorder(imgRGB,20,20,20,20,cv2.BORDER_CONSTANT,value=[255,255,255])
 
 cv2.imshow('Border'       ,imgRGBWithBorder)
